73f17feb4a6e1602a79467c1e5b49795578af4f0
Alejandro Lucas <alejandrolucas161@gmail.com>
2024-01-23 19:05:13 -0300

    setting up the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FunctionComponent } from "react";
+import styled from "styled-components";
 
-function App() {
+export const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+`;
+
+const App: FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Title>Title</Title>
+    </>
   );
-}
+};
 
 export default App;
