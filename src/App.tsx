c35d94e65961a58192fc2fa287a532ddddaae00f
Alejandro Lucas <alejandrolucas161@gmail.com>
2024-01-23 21:57:42 -0300

    Testing gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,16 @@ export const Title = styled.h1`
   font-weight: 600;
 `;
 
+export const Subtitle = styled.h3`
+  font-size: 18px;
+  font-weight: 400;
+`;
+
 const App: FunctionComponent = () => {
   return (
     <>
       <Title>Title</Title>
+      <Subtitle>Subtitle</Subtitle>
     </>
   );
 };
