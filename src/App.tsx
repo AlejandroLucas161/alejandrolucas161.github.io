b232614f7a509642ebfe94b6defa000e5e0953c5
Alejandro Lucas <alejandrolucas161@gmail.com>
2024-01-25 00:04:00 -0300

    Added example routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import { FunctionComponent } from "react";
+import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+
   width: 100vw;
   height: 100vh;
 
@@ -30,21 +37,63 @@ export const AppContainer = styled.div`
 `;
 
 export const Title = styled.h1`
-  font-size: 24px;
+  font-size: 55px;
   font-weight: 600;
+  text-align: center;
 `;
 
 export const Subtitle = styled.h3`
-  font-size: 18px;
+  font-size: 32px;
   font-weight: 400;
+  text-align: center;
+  max-width: 800px;
 `;
 
 const App: FunctionComponent = () => {
   return (
-    <AppContainer>
-      <Title>Title</Title>
-      <Subtitle>Subtitle</Subtitle>
-    </AppContainer>
+    <Router>
+      <Routes>
+        <Route
+          path="/react-ts-project"
+          element={
+            <AppContainer>
+              <Title>React TypeScript Project</Title>
+              <Subtitle>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In et
+                placerat neque, eget eleifend quam.
+              </Subtitle>
+
+              <Link to="/testRoute1">Test Route 1</Link>
+              <Link to="/testRoute2">Test Route 2</Link>
+            </AppContainer>
+          }
+        />
+
+        <Route
+          path={"/testRoute1"}
+          element={
+            <AppContainer>
+              <Title>Test Route 1</Title>
+
+              <Link to="/react-ts-project">Home</Link>
+              <Link to="/testRoute2">Test Route 2</Link>
+            </AppContainer>
+          }
+        />
+
+        <Route
+          path="/testRoute2"
+          element={
+            <AppContainer>
+              <Title>Test Route 2</Title>
+
+              <Link to="/react-ts-project">Home</Link>
+              <Link to="/testRoute1">Test Route 1</Link>
+            </AppContainer>
+          }
+        />
+      </Routes>
+    </Router>
   );
 };
 
